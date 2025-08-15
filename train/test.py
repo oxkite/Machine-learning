4cb08d9893c21aaa7cc747056803475c39eea2cc
oxkite <oxkite452879@gmail.com>
2025-08-16 03:40:36 +0900

    chore: prettier

--- train/test.py
+++ train/test.py
@@ -36,7 +36,11 @@ plt.plot(myline, mymodel(myline))
 plt.show()
 
 
-
+# 1.
+# Criminal Law - Criminal Lawyer Tel Aviv
+# 5.0(1) · Attorney
+# 10+ years in business · Tel Aviv-Yafo, Israel
+# Open ⋅ Closes 12 AM · +972 3-673-8097
 
 
 # 2.
